f9b3869d14a29d32fab9fdc16118303456d2997a
Prashant Chaturvedi <prashantchaturvedi2020@gmail.com>
2019-05-02 22:33:44 +0530

    Update basic_iteration.cpp

--- map/basic_iteration.cpp
+++ map/basic_iteration.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 #define ll long long
 int main() {
-   unordered_map<int, int> m;   // map with unsorter keys
+   unordered_map<int, int> m;   // map with unsorted keys
+   
+   int element,no_of_element;
+   
+   cin>>no_of_element;
+   
+   for(int i = 0;i < no_of_element; ++i){ cin>>element; m[element]++;} //insertion of the keys and it's frequency;
     
-   for (auto it=m.begin(); it != m.end(); ++it)
+   for (auto it = m.begin(); it != m.end(); ++it)
    {
       cout << it->first << " " << it->second<<"\n";   // basic iteration to see all th key-value pair of the map
     }
